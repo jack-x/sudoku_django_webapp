3c424243478a5c784b219f3142834a67179871f5
Naman Varma <namanvrm7@gmail.com>
2020-04-05 19:34:04 +0530

    Little changes

--- sudoku/views.py
+++ sudoku/views.py
@@ -19,7 +19,7 @@ def index(request):
 
     if request.method == 'POST':
         gettingValue = request.POST.get('solveButton')
-        checkingValue = request.POST.get('checkPuzzle')
+        checkingValue = request.POST.get('Submit')
         hintbuttonPress = request.POST.get('hintButton')
 
         if hintbuttonPress == 'Pressed':
@@ -50,7 +50,51 @@ def index(request):
 
             return render(request,'sudoku/home.html',context = sudokuDictionaryResponse)
 
+        if checkingValue == 'submitSudoku':
+            print("User has submitted")
+            userSolution = dict()
+            #extract value from page
+            for x in range(0,9):
+                for y in range(0,9):
+                    userSolution['cell'+str(x)+str(y)] = request.POST.get('cell'+str(x)+str(y))
 
+            #comparison
+            flag = True
+            for x in range(0,9):
+                for y in range(0,9):
+                    print(solutionGlobal)
+                    solutionvalue = solutionGlobal[x][y]
+                    useranswer = userSolution['cell' + str(x) + str(y)]
+                    if str(solutionvalue) != str(useranswer):
+                        flag = False
+                        break
+            if flag == True:
+                #user correct solution
+                print("User solution is correct")
+                sudokuWebKeyGen = 'cell'
+                sudokuDictionaryResponse["UserSolutionMessage"] = "Great Job you did it!!"
+                for x in range(0,9):
+                    for y in range(0,9):
+                        newKey = sudokuWebKeyGen+str(x)+str(y)
+                        if solutionGlobal[x][y] == 0:
+                            sudokuDictionaryResponse[newKey] = ' '
+                        else:
+                            sudokuDictionaryResponse[newKey] = solutionGlobal[x][y]
+                return render(request,'sudoku/home.html',context = sudokuDictionaryResponse)
+
+            else:
+                print("User solution is false")
+                sudokuDictionaryResponse["UserSolutionMessage"] = "Wrong Input! Please try again!!"
+                return render(request,'sudoku/home.html',context = sudokuDictionaryResponse)
+
+    #Clean up the global variables
+    solutionGlobal = []
+    differenceDict = dict()
+    sudokuDictionaryResponse = dict()
+
+
+    print("Page has been reloaded")
+    print("Generating new Puzzle")
     newPuzzle = Puzzler.createSudokuPuzzleWithSolution(40)
 
     puzzle = newPuzzle[0]
@@ -58,6 +102,7 @@ def index(request):
     # global solutionGlobal
     solutionGlobal = solution
 
+    print("Loading Differences")
     #load differences
 
     for x in range(0,9):
